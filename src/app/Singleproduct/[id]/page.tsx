de7cc9f11c848d31c925261a5a4e7c1ab86db808
irza <irzajaved614@gmail.com>
2025-01-03 23:13:20 +0500

    resolved errors

--- src/app/Singleproduct/[id]/page.tsx
+++ src/app/Singleproduct/[id]/page.tsx
@@ -7,26 +7,30 @@ import Link from "next/link";
 
 const AllProduct = [...AllProducts, ...R2Products, ...R3Products];
 
-
 export default function SingleProduct({ params }: { params: { id: string } }) {
   const product = AllProduct.find((p) => p.id === Number(params.id));
 
-  const featuredProducts = [7, 8, 12, 3,9].map((id) =>
-    AllProduct.find((product) => product.id === id)
-  );
-  
-  
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
+  const featuredProductIds = [7, 8, 12, 3, 9];
+  const featuredProducts = featuredProductIds
+    .map((id) => AllProduct.find((p) => p.id === id))
+    .filter(Boolean);
 
   return (
     <div className="flex flex-col items-center gap-6 p-8">
-      
-      <Image
-        src={product.image}
-        alt={product.name}
-        width={675}
-        height={607}
-        className="absolute top-[335px] left-[260px] rounded-[10px]"
-      />
+      {/* Product Details */}
+      {product.image && (
+        <Image
+          src={product.image}
+          alt={product.name}
+          width={675}
+          height={607}
+          className="absolute top-[335px] left-[260px] rounded-[10px]"
+        />
+      )}
       <span className="w-[541px] h-[132px] absolute top-[335px] left-[1044px] font-inter font-bold text-6xl leading-[66px] text-[#272343]">
         {product.name}
       </span>
@@ -36,63 +40,55 @@ export default function SingleProduct({ params }: { params: { id: string } }) {
         </span>
       </div>
       <div className="w-[543px] h-[101px] absolute top-[618px] left-[1046px] opacity-60">
-
         <p className="font-inter font-normal text-[22px] leading-[33px] text-[#272343]">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
           tincidunt erat enim. Lorem ipsum dolor sit amet, consectetur adipiscing
         </p>
       </div>
       <div className="w-[212px] h-[63px] absolute top-[751px] left-[1046px] rounded-lg px-6 py-[14px] flex justify-center items-center gap-[9px] bg-[#029FAE]">
-
         <Image src={cartlogo} alt="cartlogo" width={29} height={29} />
         <Link href="/Cart">
-        <button className="w-[116px] h-[22px] font-inter font-medium text-xl leading-[22px] text-center text-white">
-          Add to Cart
-
-          
-        </button>
+          <button className="w-[116px] h-[22px] font-inter font-medium text-xl leading-[22px] text-center text-white">
+            Add to Cart
+          </button>
         </Link>
       </div>
 
       {/* Featured Section */}
-      <div className="w-[1447px] h-[409px] absolute top-[1065px] left-[236px] ">
-
-        <span className="w-[410px] h-[34px] absolute left-[2px] font-inter text-[28px] font-bold leading-[33.89px] tracking-[5.6px] text-black ">FEATURED PRODUCTS</span>
-
-        
-
-          {featuredProducts.map((featured, index) => (
-            <div
-              key={featured.id}
-              className="w-[270px] h-[306px]  absolute top-[100px] "
-              style={{ left: index === 0 ? '0px' : `${index * 300}px`,
-
-               }}
-            >
+      <div className="w-[1447px] h-[409px] absolute top-[1065px] left-[236px]">
+        <span className="w-[410px] h-[34px] absolute left-[2px] font-inter text-[28px] font-bold leading-[33.89px] tracking-[5.6px] text-black">
+          FEATURED PRODUCTS
+        </span>
+        {featuredProducts.map((featured, index) => (
+          <div
+            key={featured?.id}
+            className="w-[270px] h-[306px] absolute top-[100px]"
+            style={{ left: index === 0 ? "0px" : `${index * 300}px` }}
+          >
+            {featured?.image && (
               <Image
                 src={featured.image}
                 alt={featured.name}
                 width={270}
                 height={263}
-                className="rounded-[10px] "
+                className="rounded-[10px]"
               />
-              <span className="w-[141px] h-[21px] absolute top-[288px] left-[2px] text-base leading-[20.8px] text-[#272343]">
-                Libarary Stool Chair</span>
-
-              <span className="w-7 h-[17px] absolute top-[288px] left-[235px] text-sm leading-[16.94px] text-black font-inter font-bold">$99</span>
-            </div>
-          ))}
-        
-        <div className="w-[75px] h-[35px] absolute top-[3px] left-[1353px] ">
-
+            )}
+            <span className="w-[141px] h-[21px] absolute top-[288px] left-[2px] text-base leading-[20.8px] text-[#272343]">
+              {featured?.name}
+            </span>
+            <span className="w-7 h-[17px] absolute top-[288px] left-[235px] text-sm leading-[16.94px] text-black font-inter font-bold">
+              $99
+            </span>
+          </div>
+        ))}
+        <div className="w-[75px] h-[35px] absolute top-[3px] left-[1353px]">
           <Link href="/Product">
-
-            <span className="w-[75px] h-[22px] text-black font-inter font-bold text-lg leading-[21.78px]"> 
+            <span className="w-[75px] h-[22px] text-black font-inter font-bold text-lg leading-[21.78px]">
               View All
             </span>
           </Link>
-
-          <span className = "w-[75px] h-[2px] absolute top-[34px] left-[1px] bg-black "></span>
+          <span className="w-[75px] h-[2px] absolute top-[34px] left-[1px] bg-black"></span>
         </div>
       </div>
     </div>
